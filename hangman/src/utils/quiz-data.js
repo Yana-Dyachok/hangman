762871d0d7dcc18830377c10c23aaf162f6769cf
Yana-Dyachok <annetart1994@gmail.com>
2023-12-27 23:21:58 +0200

    feat: add data to quiz-data

---
+++ hangman/src/utils/quiz-data.js
@@ -0,0 +1,158 @@
+export const quizData = [
+    {
+        question: "What is the term for a tall plant with a large yellow flower head?",
+        answer: "sunflower",
+    },
+    {
+        question: "Describe a dense forest characterized by high rainfall and biodiversity.",
+        answer: "rainforest",
+    },
+    {
+        question: "What is the name of a building or outdoor area where plays, movies, or other performances are staged?",
+        answer: "theater",
+    },
+    {
+        question: "What is a device used to transmit sound over long distances?",
+        answer: "telephone",
+    },
+    {
+        question: "Define the term: A system of communication consisting of words, gestures, and syntax.",
+        answer: "language",
+    },
+    {
+        question: "What is a barren or arid land with little or no precipitation?",
+        answer: "desert",
+    },
+    {
+        question: "An optical instrument used to view distant objects in space.",
+        answer: "telescope",
+    },
+    {
+        question: "What is a gentle wind called?",
+        answer: "breeze",
+    },
+    {
+        question: "What is the study of celestial objects and phenomena beyond Earth's atmosphere?",
+        answer: "astronomy",
+    },
+    {
+        question: "In music, what is a lengthy musical composition for a full orchestra, typically in multiple movements?",
+        answer: "symphony",
+    },
+    {
+        question: "What is a long work of fiction, typically with a complex plot and characters?",
+        answer: "novel",
+    },
+    {
+        question: "What is the art and science of designing and constructing buildings?",
+        answer: "architecture",
+    },
+    {
+        question: "In classical dance, what form is characterized by precise and graceful movements?",
+        answer: "ballet",
+    },
+    {
+        question: "Who is a person trained to travel and work in space?",
+        answer: "astronaut",
+    },
+    {
+        question: "What is a cascade of water falling from a height?",
+        answer: "waterfall",
+    },
+    {
+        question: "What is the application of scientific knowledge for practical purposes?",
+        answer: "technology",
+    },
+    {
+        question: "What is a meteorological phenomenon caused by reflection, refraction, and dispersion of light?",
+        answer: "rainbow",
+    },
+    {
+        question: "What encompasses all existing matter, space, and time as a whole?",
+        answer: "universe",
+    },
+    {
+        question: "The art, process, or practice of creating images by recording light or other electromagnetic radiation.",
+        answer: "photography",
+    },
+    {
+        question: "What is a celestial body that orbits a star and does not produce light of its own?",
+        answer: "planet",
+    },
+    {
+        question: "What is a large natural stream of water flowing in a channel to the sea, a lake, or another such stream?",
+        answer: "river",
+    },
+    {
+        question: "Relating to or situated in the region between the Tropic of Cancer and the Tropic of Capricorn.",
+        answer: "tropical",
+    },
+    {
+        question: "Difficult or impossible to understand, explain, or identify.",
+        answer: "mysterious",
+    },
+    {
+        question: "What is the genre of imaginative fiction involving magic and supernatural elements?",
+        answer: "fantasy",
+    },
+    {
+        question: "What is a statement or situation that contradicts itself or defies intuition?",
+        answer: "paradox",
+    },
+    {
+        question: "What game, toy, or problem is designed to test ingenuity or knowledge?",
+        answer: "puzzle",
+    },
+    {
+        question: "To speak very softly or quietly, often in a secretive manner.",
+        answer: "whisper",
+    },
+    {
+        question: "What is a dark area or shape produced by an object blocking the light?",
+        answer: "shadow",
+    },
+    {
+        question: "Something kept hidden or unknown to others.",
+        answer: "secret",
+    },
+    {
+        question: "What is a strong desire to know or learn something?",
+        answer: "curiosity",
+    },
+    {
+        question: "Not able to be foreseen or known beforehand; uncertain.",
+        answer: "unpredictable",
+    },
+    {
+        question: "To confuse or bewilder someone; to make something unclear or difficult to understand.",
+        answer: "obfuscate",
+    },
+    {
+        question: "To make known or reveal something previously secret or unknown.",
+        answer: "unveil",
+    },
+    {
+        question: "A false perception or belief; a deceptive appearance or impression.",
+        answer: "illusion",
+    },
+    {
+        question: "The light from the moon.",
+        answer: "moonlight",
+    },
+    {
+        question: "Full of energy, brightness, and life.",
+        answer: "vibrant",
+    },
+    {
+        question: "A sentimental longing or wistful affection for the past.",
+        answer: "nostalgia",
+    },
+    {
+        question: "Exceptionally clever, talented, or impressive.",
+        answer: "brilliant",
+    },
+    {
+        question: "An expedition or journey, typically to observe wildlife in their natural habitat.",
+        answer: "safari",
+    },
+];
