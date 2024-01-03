4c184f3e25da44d77ea04e0696b01c7ea3f4806d
Yana-Dyachok <annetart1994@gmail.com>
2024-01-03 10:28:04 +0200

    feat: add prettier

--- hangman/src/utils/quiz.js
+++ hangman/src/utils/quiz.js
@@ -6,33 +6,30 @@ let prevTempNumb = getLocalStorage() || 0;
 export let rightAnswer = "";
 
 export function getRandomWord() {
-    tempNumb = Math.floor(Math.random() * (quizData.length - 1));
-    setLocalStorage();
-    if (tempNumb === prevTempNumb) tempNumb += 1;
-    const { question, answer } = quizData[tempNumb];
-    quizQuestion.innerText = question;
-    rightAnswer = answer.toUpperCase();
-    console.log(`Right answer is - "${rightAnswer}"`);
-    secretWord.innerHTML = answer
-        .split("")
-        .map(() => `<li class="quiz__letter">_</li>`)
-        .join("");
+  tempNumb = Math.floor(Math.random() * (quizData.length - 1));
+  setLocalStorage();
+  if (tempNumb === prevTempNumb) tempNumb += 1;
+  const { question, answer } = quizData[tempNumb];
+  quizQuestion.innerText = question;
+  rightAnswer = answer.toUpperCase();
+  console.log(`Right answer is - "${rightAnswer}"`);
+  secretWord.innerHTML = answer
+    .split("")
+    .map(() => `<li class="quiz__letter">_</li>`)
+    .join("");
 }
 
 getRandomWord();
 
-
 // save random quiz in localStorage ----------------------------------------------------------------------------------------------------------
 function setLocalStorage() {
-    localStorage.setItem("tempNumb", tempNumb);
+  localStorage.setItem("tempNumb", tempNumb);
 }
-
 
 function getLocalStorage() {
-    const storedValue = localStorage.getItem("tempNumb");
-    return parseInt(storedValue, 10);
+  const storedValue = localStorage.getItem("tempNumb");
+  return parseInt(storedValue, 10);
 }
-
 
 window.addEventListener("load", getLocalStorage);
 window.addEventListener("beforeunload", setLocalStorage);
